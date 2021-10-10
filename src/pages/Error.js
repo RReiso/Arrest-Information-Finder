22fdc52587a018607230d8c99a7eaab2f217e864
Ruta R <rutareiso@protonmail.com>
2021-10-10 06:32:48 -0400

    Style error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -5,11 +5,11 @@ const Error = () => {
 	return (
 		<section className="section error">
 			<p className="error-statement">Oops! Page not found!</p>
-			<p className="404">404</p>
+			<p className="n404">404</p>
 			<p className="error-statement">
 				We are sorry, but the page you requested was not found!
 			</p>
-			<Link to="/">Back to Home</Link>
+			<Link className="link" to="/">Back to Home</Link>
 		</section>
 	);
 };
