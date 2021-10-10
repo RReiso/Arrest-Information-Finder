5a515d93339a629a071f62e483f269cc6ebc0118
Ruta R <rutareiso@protonmail.com>
2021-10-10 07:00:32 -0400

    Add app name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,8 +17,14 @@ const Navbar = () => {
 				>
 					&#9776;
 				</button>
+				<Link className="nav-title" to="/">
+					Arrest Info Finder
+				</Link>
 			</div>
 			<nav id="navbar" className={isOpen === true ? "nav showNav" : "nav"}>
+				<Link className="nav-title second" to="/">
+					Arrest Info Finder
+				</Link>
 				<ul className="nav-list">
 					<li className="nav-link" onClick={toggleHamburger}>
 						<Link to="/">Home</Link>
